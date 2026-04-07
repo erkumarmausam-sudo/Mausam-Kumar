4018892f5c76644898ffea9add2db2dfbad2cd85
Mausam-Kumar <er.kumar.mausam@gmail.com>
2026-04-07 11:59:57 +0530

    all experiment

---
+++ JS/dom.js
@@ -0,0 +1,14 @@
+// document.writeln("hello");
+
+function result(){
+    let inputData=document.getElementById("InputText").value ;
+    document.getElementById('text').textContent=inputData;
+
+    let changeColor=document.getElementById('text');
+        changeColor.style.color="red";
+
+    let bodyColor = document.
+    getElementByName('body')[0];
+    bodyColor.style.backgroundColor="blue";
+
+}
